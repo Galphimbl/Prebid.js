da9945fd217ccdccbc34056ffd2aa0ff2c70cf10
Galphimbl <sashatkachov91@gmail.com>
2020-08-13 12:32:48 +0300

    analytics enable

--- bundle-service/loader.js
+++ bundle-service/loader.js
@@ -2,7 +2,9 @@
   var siteConfig = JSON.parse(document.currentScript.textContent || '{}');
   var AD_UNITS = JSON.parse("%%AD_UNITS%%");
   var PREBID_CDN = siteConfig.prebid_cdn || "%%PREBID_CDN%%";
+  var PREBID_URL = siteConfig.prebid_url || "%%PREBID_URL%%"
   var PREBID_TIMEOUT = siteConfig.prebid_timeout || parseInt("%%PREBID_TIMEOUT%%");
+  var PRICE_GRANULARITY = siteConfig.price_granularity || JSON.parse("%%PRICE_GRANULARITY%%");
   var adapters = Object.keys(
     AD_UNITS.reduce(function(obj, adUnit) {
       adUnit.bids.forEach(function(bid){
@@ -11,11 +13,12 @@
       return obj;
     }, {})
   ).sort();
-  var analytics = ['admixer'];
+  var analytics = ['admixer'].sort();
   var pbjs = window.pbjs = window.pbjs || {};
   pbjs.que = pbjs.que || [];
   var script = document.createElement('script');
-  script.src = PREBID_CDN + '/prebid.js?ba=' + adapters.join(",") + "&aa=" + analytics.join(",");
+  var prebidUrl = PREBID_URL ? PREBID_URL : (PREBID_CDN + "/prebid.js");
+  script.src = prebidUrl + "?ba=" + adapters.join(",") + "&aa=" + analytics.join(",");
   document.head.appendChild(script);
 
   pbjs.que.push(function() {
@@ -23,10 +26,13 @@
       provider: 'admixer',
       options: {options: true}
     });
-    pbjs.addAdUnits(AD_UNITS);
+    pbjs.setConfig({
+      priceGranularity: PRICE_GRANULARITY
+    });
     pbjs.requestBids({
       bidsBackHandler: sendAdserverRequest,
-      timeout: PREBID_TIMEOUT
+      timeout: PREBID_TIMEOUT,
+      adUnits: AD_UNITS,
     });
   });
 
