a6e0f569141bf9ce132359b9093bdfb1e89098de
Galphimbl <sashatkachov91@gmail.com>
2020-11-30 17:34:20 +0200

    addd port

--- bundle-service/index.js
+++ bundle-service/index.js
@@ -1,6 +1,7 @@
 const {prebidJSRoute} = require("./routes/prebid.js");
 const express = require('express');
 const bodyParser = require('body-parser');
+const argv = require('yargs').argv;
 /**
  *
  * @param {IncomingMessage} req
@@ -12,7 +13,7 @@ app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/prebid.js', prebidJSRoute);
-const server = app.listen(3000, function () {
+const server = app.listen(argv.port || 3000, function () {
   console.log('Listening on port %s...', server.address().port);
 });
 
