cb5f16ddcc26eae749da0343f8387ec8d2a9aa13
Galphimbl <sashatkachov91@gmail.com>
2020-12-08 17:30:48 +0200

    prebid.js -> prebidcdn.js

--- bundle-service/index.js
+++ bundle-service/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/prebid.js', prebidJSRoute);
+app.use('/prebidcdn.js', prebidJSRoute);
 const server = app.listen(argv.port || 3000, function () {
   console.log('Listening on port %s...', server.address().port);
 });
